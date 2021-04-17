7a29eaf03ddf1105f49eca7975905f41ab1c1123
Dana Woltz <dw952717@ohio.edu>
2021-04-17 14:33:42 -0400

    Game Updatwe

--- Accelerate!/Assets/PlatformControlller.cs
+++ Accelerate!/Assets/PlatformControlller.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class PlatformControlller : MonoBehaviour
 {
     public bool horizontalCheck;
     public bool isAtTarget;
 
-    public Transform posTarget;
-    public Transform negTarget;
+    public Transform posXTarget;
+    public Transform negXTarget;
+
+    public Transform posYTarget;
+    public Transform negYTarget;
 
     public float speed;
-    public float delta;
+
+    private float boolRandomizer;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(4, 20);
+        boolRandomizer = Random.value;
+
+        if(boolRandomizer >= 0.5f)
+        {
+            horizontalCheck = true;
+        }
+        else if (boolRandomizer <= 0.5f)
+        {
+            horizontalCheck = false;
+        }
     }
 
     // Update is called once per frame
@@ -32,8 +47,8 @@ public class PlatformControlller : MonoBehaviour
         {
             if (!isAtTarget)
             {
-                transform.position = Vector3.MoveTowards(transform.position, posTarget.position, speed * Time.deltaTime);
-                if (transform.position.x >= posTarget.position.x)
+                transform.position = Vector3.MoveTowards(transform.position, posXTarget.position, speed * Time.deltaTime);
+                if (transform.position.x >= posXTarget.position.x)
                 {
                     isAtTarget = true;
                 }
@@ -41,8 +56,8 @@ public class PlatformControlller : MonoBehaviour
 
             if (isAtTarget)
             {
-                transform.position = Vector3.MoveTowards(transform.position, negTarget.position, speed * Time.deltaTime);
-                if (transform.position.x <= negTarget.position.x)
+                transform.position = Vector3.MoveTowards(transform.position, negXTarget.position, speed * Time.deltaTime);
+                if (transform.position.x <= negXTarget.position.x)
                 {
                     isAtTarget = false;
                 }
@@ -53,8 +68,8 @@ public class PlatformControlller : MonoBehaviour
         {
             if (!isAtTarget)
             {
-                transform.position = Vector3.MoveTowards(transform.position, posTarget.position, speed * Time.deltaTime);
-                if (transform.position.y >= posTarget.position.y)
+                transform.position = Vector3.MoveTowards(transform.position, posYTarget.position, speed * Time.deltaTime);
+                if (transform.position.y >= posYTarget.position.y)
                 {
                     isAtTarget = true;
                 }
@@ -62,8 +77,8 @@ public class PlatformControlller : MonoBehaviour
 
             if (isAtTarget)
             {
-                transform.position = Vector3.MoveTowards(transform.position, negTarget.position, speed * Time.deltaTime);
-                if (transform.position.y <= negTarget.position.y)
+                transform.position = Vector3.MoveTowards(transform.position, negYTarget.position, speed * Time.deltaTime);
+                if (transform.position.y <= negYTarget.position.y)
                 {
                     isAtTarget = false;
                 }
